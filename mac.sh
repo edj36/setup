1ca30b02a553ae4fdd22e425c250186ec77f84ad
edj36 <edj36@cornell.edu>
2019-08-09 01:18:40 -0400

    update script more

--- mac.sh
+++ mac.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # TODO git ssh key stuff here
+# echo "Creating an SSH key for you..."
+# ssh-keygen -t rsa
+
+# echo "Please add this public key to Github \n"
+# echo "https://github.com/account/ssh \n"
+# read -p "Press [Enter] key after this..."
 
 echo "installing xcode stuff"
 xcode-select --install
@@ -15,13 +21,26 @@ fi
 echo "updating homebrew"
 brew update
 
+echo "installing git and setting config"
+brew install git
+git config --global user.name edj36
+git config --global user.email edj36@cornell.edu
+
 echo "installing tools with homebrew"
 brew install python
 brew install jupyter
 brew install ipython
 brew install pandoc
-brew install git
 
 echo "installing cask to get latex"
 brew tap caskroom/cask
 brew cask install mactex
+
+echo "cleaning up"
+
+brew cask cleanup
+brew cleanup
+
+echo "done"
+
+
